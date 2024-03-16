f0cd98eb539a4a8f5d2cc97b57353f34f62a26aa
Markus Moenig <markus@moenig.io>
2024-03-16 14:39:46 +0700

    Trying out the new menu

--- creator/src/editor.rs
+++ creator/src/editor.rs
@@ -151,10 +151,42 @@ impl TheTrait for Editor {
 
         ui.set_statusbar_name("Statusbar".to_string());
 
+        // Menu
+
+        let mut menu_canvas = TheCanvas::new();
+        let mut menu = TheMenu::new(TheId::named("Menu"));
+
+        let mut file_menu = TheContextMenu::named(str!("File"));
+        file_menu.add(TheContextMenuItem::new(
+            str!("Open..."),
+            TheId::named("Open"),
+        ));
+        file_menu.add(TheContextMenuItem::new(str!("Save"), TheId::named("Save")));
+        file_menu.add(TheContextMenuItem::new(
+            str!("Save As ..."),
+            TheId::named("Save As"),
+        ));
+        let mut edit_menu = TheContextMenu::named(str!("Edit"));
+        edit_menu.add(TheContextMenuItem::new(str!("Undo"), TheId::named("Undo")));
+        edit_menu.add(TheContextMenuItem::new(str!("Redo"), TheId::named("Redo")));
+        edit_menu.add_separator();
+        edit_menu.add(TheContextMenuItem::new(str!("Cut"), TheId::named("Cut")));
+        edit_menu.add(TheContextMenuItem::new(str!("Copy"), TheId::named("Copy")));
+        edit_menu.add(TheContextMenuItem::new(
+            str!("Paste"),
+            TheId::named("Paste"),
+        ));
+
+        menu.add_context_menu(file_menu);
+        menu.add_context_menu(edit_menu);
+
+        menu_canvas.set_widget(menu);
+
         // Menubar
         let mut top_canvas = TheCanvas::new();
 
-        let menubar = TheMenubar::new(TheId::named("Menubar"));
+        let mut menubar = TheMenubar::new(TheId::named("Menubar"));
+        menubar.limiter_mut().set_max_height(43 + 22);
 
         let mut logo_button = TheMenubarButton::new(TheId::named("Logo"));
         logo_button.set_icon_name("logo".to_string());
@@ -241,6 +273,7 @@ impl TheTrait for Editor {
 
         top_canvas.set_widget(menubar);
         top_canvas.set_layout(hlayout);
+        top_canvas.set_top(menu_canvas);
         ui.canvas.set_top(top_canvas);
 
         // Sidebar
