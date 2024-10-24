798e43be0982f583916affe6b707631ac597ae26
Markus Moenig <markus@moenig.io>
2024-10-24 15:06:34 +0700

    Update _lib.rs

--- creator/src/_lib.rs
+++ creator/src/_lib.rs
@@ -6,13 +6,17 @@ pub mod brushes;
 pub mod brushlist;
 pub mod editor;
 pub mod externals;
+pub mod materialeditor;
+pub mod minimap;
 pub mod misc;
+pub mod modeleditor;
 pub mod modelfxeditor;
 pub mod panels;
 pub mod regionfxeditor;
 pub mod screeneditor;
 pub mod self_update;
 pub mod sidebar;
+pub mod terraineditor;
 pub mod tileeditor;
 pub mod tilefxeditor;
 pub mod tilemapeditor;
@@ -28,6 +32,8 @@ use rust_embed::RustEmbed;
 #[exclude = "*.DS_Store"]
 pub struct Embedded;
 
+const DEFAULT_VLAYOUT_RATIO: f32 = 0.62;
+
 pub mod prelude {
 
     pub use ::serde::{Deserialize, Serialize};
@@ -38,12 +44,15 @@ pub mod prelude {
 
     pub use crate::brushlist::*;
     pub use crate::externals::*;
+    pub use crate::materialeditor::*;
     pub use crate::misc::*;
+    pub use crate::modeleditor::*;
     pub use crate::modelfxeditor::*;
     pub use crate::panels::*;
     pub use crate::regionfxeditor::*;
     pub use crate::screeneditor::*;
     pub use crate::sidebar::*;
+    pub use crate::terraineditor::*;
     pub use crate::tileeditor::*;
     pub use crate::tilefxeditor::*;
     pub use crate::tilemapeditor::*;
@@ -60,13 +69,20 @@ pub mod prelude {
     pub use crate::tools::fx::FXTool;
     pub use crate::tools::game::GameTool;
     pub use crate::tools::mapobjects::MapObjectsTool;
+    pub use crate::tools::model::edit::ModelNodeEditTool;
     pub use crate::tools::picker::PickerTool;
     pub use crate::tools::render::RenderTool;
+    pub use crate::tools::resize::ResizeTool;
     pub use crate::tools::screen::eraser::ScreenEraserTool;
     pub use crate::tools::screen::game::ScreenGameTool;
+    pub use crate::tools::screen::material::edit::MaterialNodeEditTool;
     pub use crate::tools::screen::picker::ScreenPickerTool;
     pub use crate::tools::screen::tiledrawer::ScreenTileDrawerTool;
     pub use crate::tools::selection::SelectionTool;
+    pub use crate::tools::terrain::draw::TerrainDrawTool;
+    pub use crate::tools::terrain::height::TerrainHeightTool;
+    pub use crate::tools::terrain::selection::TerrainSelectionTool;
+    pub use crate::tools::terrain::zoom::TerrainZoomTool;
     pub use crate::tools::tiledrawer::TileDrawerTool;
     pub use crate::tools::tilemap::TilemapTool;
     pub use crate::tools::zoom::ZoomTool;
